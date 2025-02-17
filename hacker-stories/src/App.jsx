06089d57025ca7496510b3fda37f9e1fd1186a6b
Hamza <mighrihamza60@gmail.com>
2025-02-17 16:25:21 +0100

    W2 My first React component

---
+++ hacker-stories/src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+function App() {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
+}
+export default App;
